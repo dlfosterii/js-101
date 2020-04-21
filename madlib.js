179921436bb350e2ebcdae3b1986d8edfdc3a41a
David Foster <dlfosterii@yahoo.com>
2020-04-21 07:43:35 -0400

    exercise 3 complete

---
+++ madlib.js
@@ -0,0 +1,11 @@
+// function madLib(name, subject) {
+//     console.log(name +"'s faviorite subject is " + subject);
+
+// }
+// madLib('Anushka', 'art')
+
+function madLib(name, subject) {
+    return `${name}'s favorite subject is ${subject}.`
+}
+madLib('David', 'Band')
+console.log (madLib)
